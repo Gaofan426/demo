4cec5566e918ff623c083ba93cff7924513badc2
gaof@chinacaring.com <FANRU@426>
2019-07-25 10:36:23 +0800

    更新 basic认证

--- src/main/java/com/van/demo/DemoApplication.java
+++ src/main/java/com/van/demo/DemoApplication.java
@@ -1,9 +1,15 @@
 package com.van.demo;
 
+import com.van.demo.config.HTTPBasicAuthorizeAttribute;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication(scanBasePackages = "com.van")
 @EnableFeignClients("com.van")
@@ -12,6 +18,17 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        HTTPBasicAuthorizeAttribute httpBasicFilter = new HTTPBasicAuthorizeAttribute();
+        registrationBean.setFilter(httpBasicFilter);
+        List<String> urlPatterns = new ArrayList<>(16);
+        urlPatterns.add("/*");
+        registrationBean.setUrlPatterns(urlPatterns);
+        return registrationBean;
     }
 
 }
---
+++ src/main/java/com/van/demo/dto/ResultStatusCode.java
@@ -0,0 +1,5 @@
+package com.van.demo.dto;
+
+public enum ResultStatusCode {
+    OK,PERMISSION_DENIED
+}
